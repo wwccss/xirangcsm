f734c79a9b47a8d59ec30db4ba6f1020d9918b11
wyd621 <wyd621@163.com>
2013-05-22 11:05:52 +0800

    * change iframe's style for footer.

--- module/common/view/footer.html.php
+++ module/common/view/footer.html.php
@@ -1,6 +1,6 @@
   </div>
   <div class='a-center' id='copyright'><?php printf($lang->poweredBy, $config->version);?></div>
-  <div class='row'><div class='u-1'><iframe name='hiddenwin' id='hiddenwin' class='<?php $config->debug ? print('debugwin') : print('hidden');?>'></iframe></div></div>
+  <iframe frameborder='0' scrolling='no' name='hiddenwin' id='hiddenwin' class='<?php $config->debug ? print('debugwin') : print('hidden');?>'></iframe>
 </div>
 <script laguage='Javascript'>
 <?php if(isset($pageJS)) echo $pageJS;?>
