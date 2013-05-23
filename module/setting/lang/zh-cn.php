8a4306195bcd4a9f50f76bf9ff2dcff647cfc029
wyd621 <wyd621@163.com>
2013-05-23 13:27:52 +0800

    * change lang for setting.

--- module/setting/lang/zh-cn.php
+++ module/setting/lang/zh-cn.php
@@ -3,7 +3,7 @@ $lang->setting->common    = '系统配置';
 $lang->setting->setConfig = '同步配置';
 
 $lang->setting->syncConfig = '同步配置';
-$lang->setting->isSync     = '同步PMS';
+$lang->setting->isSync     = '同步禅道';
 $lang->setting->key        = '验证密钥';
 $lang->setting->IP         = '允许IP';
 $lang->setting->noIP       = '不限IP';
