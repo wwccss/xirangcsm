52c0f385f08f01fc3bc38cdd40f34ac113899ace
wyd621 <wyd621@163.com>
2013-05-23 09:48:55 +0800

    * adjust style for user-login.

--- module/user/view/login.admin.html.php
+++ module/user/view/login.admin.html.php
@@ -10,46 +10,15 @@
  */
 ?>
 <?php include '../../common/view/header.lite.html.php';?>
-<style type="text/css">
-  body {
-    padding-top: 40px;
-    padding-bottom: 40px;
-    background-color: #f5f5f5;
-   }
-  .form-signin {
-    max-width: 300px;
-    padding: 19px 29px 29px;
-    margin: 0 auto 20px;
-    background-color: #fff;
-    border: 1px solid #e5e5e5;
-    -webkit-border-radius: 5px;
-       -moz-border-radius: 5px;
-            border-radius: 5px;
-    -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
-       -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
-            box-shadow: 0 1px 2px rgba(0,0,0,.05);
-  }
-  .form-signin .form-signin-heading,
-  .form-signin .checkbox {
-    margin-bottom: 10px;
-  }
-  .form-signin input[type="text"],
-  .form-signin input[type="password"] {
-    font-size: 16px;
-    height: auto;
-    margin-bottom: 15px;
-    padding: 7px 9px;
-  }
-</style>
 <script language='Javascript'>
 $(document).ready(function(){$('#account').focus();})
 </script>
 <div class="container">
   <form class="form-signin" target='hiddenwin' method='post'>
-    <h2 class="form-signin-heading"><?php echo $lang->zentaoasm?></h2>
-    <input type="text" name='account' class="input-block-level" placeholder="<?php echo $lang->user->account?>">
+    <h3 class="form-signin-heading"><?php echo $lang->zentaoasm?></h3>
+    <input type="text" id='account' name='account' class="input-block-level" placeholder="<?php echo $lang->user->account?>">
     <input type="password" name='password' class="input-block-level" placeholder="<?php echo $lang->user->password?>">
-    <button class="btn btn-large btn-primary" type="submit"><?php echo $lang->user->login->common?></button>
+    <button class="btn btn-primary" type="submit"><?php echo $lang->user->login->common?></button>
   </form>
 </div>
 <p align='center'><?php echo html::a('http://www.zentao.net/goto.php?item=zentaoasm', sprintf($lang->poweredBy, $config->version), '_blank');?></p>
--- module/user/view/login.front.html.php
+++ module/user/view/login.front.html.php
@@ -1,5 +1,29 @@
 <?php include '../../common/view/header.html.php';?>
-<div>
-  <?php include './blockuserform.html.php';?>
-</div>  
+<script language='Javascript'>
+$(document).ready(function(){$('#account').focus();})
+</script>
+<div class="container">
+  <?php
+  if(empty($referer))
+  {
+     if(!$this->server->http_referer or 
+         strpos($this->server->http_referer, $app->site->domain) == false or 
+         strpos($this->server->http_referer, 'log') != false)
+     {
+         $referer = urlencode($this->createLink('user', 'myService'));
+     }    
+     else
+     {
+         $referer = $this->server->http_referer;
+     }
+  }   
+  ?>
+  <form class="form-signin" target='hiddenwin' method='post'>
+    <h3 class="form-signin-heading"><?php $this->session->user->account == 'guest' ? print($lang->dashboard) : printf($lang->welcome, $this->session->user->account);?></h3>
+    <input type="text" id='account' name='account' class="input-block-level" placeholder="<?php echo $lang->user->account?>">
+    <input type="password" name='password' class="input-block-level" placeholder="<?php echo $lang->user->password?>">
+    <button class="btn btn-primary" type="submit"><?php echo $lang->user->login->common?></button>
+    <input type='hidden' value='<?php echo $referer;?>' name='referer' />
+  </form>
+</div>
 <?php include '../../common/view/footer.html.php';?>
